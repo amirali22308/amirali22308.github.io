3c54899b352016ad066bec2d9a9cb50844130250
amirali dev <amirh22308@gmail.com>
2021-11-30 19:16:48 +0330

    new upload

---
+++ script/main.js
@@ -0,0 +1,11 @@
+function log_in(){
+    localStorage.setItem("loged",1);
+    window.location.href = "../pages/home.html";
+}
+function on_load(){
+    var loged = localStorage.getItem("loged");
+    if (loged != 1){
+        var newUrl = "not_loged_in.html";
+        window.location.href = newUrl;
+    }
+}
